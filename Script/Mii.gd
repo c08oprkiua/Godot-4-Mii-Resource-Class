0090ef514930b8a89cc8e600a5d24d1fe0ecb666
c08oprkiua <85906487+c08oprkiua@users.noreply.github.com>
2023-11-13 12:08:23 -0700

    Update Mii.gd

--- Script/Mii.gd
+++ Script/Mii.gd
@@ -15,76 +15,82 @@ class_name MiiDataResource
 @export var dsi: bool
 @export var special: bool
 @export var mac_address: String
+@export var creator_id: String #From FFLiCharInfo
+@export var author_id: String #From FFLiCharInfo
+
 
 @export_group("General")
 @export_enum("Male", "Female") var sex: int
 @export var birthday_month: int
 @export var birthday_day: int
-@export var favorite_color: int
+@export_range(0, 12) var favorite_color: int
 @export var favorite_mii: bool
 @export var mii_name: String 
-@export var width: float
-@export var height: float
+@export var body_width: int
+@export var body_height: int
 @export var sharable: bool
 @export var author: String
 
 @export_group("Face Misc.")
-@export var face_shape: int
+@export_range(0, 12) var face_shape: int
 @export var skin_color: int
+@export_range(0, 12) var face_line: int #from FFLiCharInfo
+@export_range(0, 6) var face_line_color: int #from FFLiCharInfo
 @export var wrinkles: int
 @export var makeup: int
 
 @export_group("Hair")
-@export var hair_style: int
-@export var hair_color: int
-@export var hair_flip: int
+@export_range(0, 132) var hair_style: int
+@export_range(0, 8) var hair_color: int
+@export var hair_flip: int #bool?
 
 @export_group("Eyes")
-@export var eye_style: int
-@export var eye_color: int
-@export var eye_scale: int
-@export var eye_y_scale: int
-@export var eye_rotation: int
-@export var eye_x_spacing: int
-@export var eye_y_position: int
+@export_range(0, 60) var eye_style: int
+@export_range(0, 6) var eye_color: int
+@export_range(0, 8) var eye_scale: int
+@export_range(0, 7) var eye_y_scale: int
+@export_range(0, 8) var eye_rotation: int
+@export_range(0, 13) var eye_x_spacing: int
+@export_range(0, 19) var eye_y_position: int
 
 @export_group("Eyebrows")
 @export_range(0, 31) var eyebrow_style: int
-@export var eyebrow_color: int
-@export var eyebrow_scale: int
-@export var eyebrow_y_scale: int
-@export var eyebrow_rotation: int
-@export var eyebrow_x_spacing: int
-@export var eyebrow_y_position: int
+@export_range(0, 8) var eyebrow_color: int
+@export_range(0, 9) var eyebrow_scale: int
+@export_range(0, 7) var eyebrow_y_scale: int
+@export_range(0, 12) var eyebrow_rotation: int
+@export_range(0, 13) var eyebrow_x_spacing: int
+@export_range(3,19) var eyebrow_y_position: int
 
 @export_group("Nose")
-@export var nose_style: int
-@export var nose_scale: int
-@export var nose_y_position: int
+@export_range(0, 18) var nose_style: int
+@export_range(0, 9) var nose_scale: int
+@export_range(0, 19) var nose_y_position: int
 
 @export_group("Mouth")
-@export var mouth_style: int
-@export var mouth_color: int
-@export var mouth_scale:int
-@export var mouth_y_scale: int
-@export var mouth_y_position: int
+@export_range(0, 36) var mouth_style: int
+@export_range(0, 5) var mouth_color: int
+@export_range(0, 9) var mouth_x_scale:int
+@export_range(0, 7) var mouth_y_scale: int
+@export_range(0, 7) var mouth_y_position: int
 
 @export_group("Facial Hair")
-@export var beard_style: int
-@export var beard_color: int
-@export var mustache_scale: int
-@export var mustache_y_position: int
+@export_range(0, 6) var beard_style: int
+@export_range(0, 8) var beard_color: int
+@export_range(0, 6) var mustache_style: int
+@export_range(0, 9) var mustache_scale: int
+@export_range(0, 17) var mustache_y_position: int
 
 @export_group("Glasses")
-@export var glasses_style: int
-@export var glasses_color: int
-@export var glasses_scale: int
-@export var glasses_y_position: int
+@export_range(0, 9) var glasses_style: int
+@export_range(0, 6) var glasses_color: int
+@export_range(0, 8) var glasses_scale: int
+@export_range(0, 21) var glasses_y_position: int
 
 @export_group("Mole")
 @export var mole_enabled: bool
-@export_range(0, 15) var mole_scale: int
-@export_range(0, 31) var mole_x_position: int
+@export_range(0, 9) var mole_scale: int
+@export_range(0, 17) var mole_x_position: int
 @export_range(0, 31) var mole_y_position: int
 
 #Essentially a full code implementation of https://www.3dbrew.org/wiki/Mii#Mii_format
